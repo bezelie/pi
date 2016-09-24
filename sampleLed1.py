e63ef2a3ee7db5c38a3109725cb87f3a3e9e3733
bezelie <nuja@live.jp>
2016-09-24 23:35:12 +0900

    Sept 24th 2016

---
+++ sampleLed1.py
@@ -0,0 +1,21 @@
+# Bezelie Sample Code for Raspberry Pi : LED test
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+# Definition
+led_pin = 24       # GPIO 24
+
+# Set Up
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led_pin, GPIO.OUT)
+
+# Main Loop
+try:
+  while True:
+    GPIO.output (led_pin, True)
+    sleep(0.5)
+    GPIO.output (led_pin, False)
+    sleep(0.5)
+except KeyboardInterrupt:
+  print " Interrupted by Keyboard"
