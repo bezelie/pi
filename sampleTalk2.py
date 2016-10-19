b6fddd98d0c89cd77119034c1d547c855b40ab77
bezelie <nuja@live.jp>
2016-10-20 07:30:57 +0900

    Oct20th2016

--- sampleTalk2.py
+++ sampleTalk2.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*- Bezelie Sample Code for Raspberry Pi selecting 
-# messages from csv file
+# speaking messages referring to csv file
 
 import csv
 import datetime
@@ -9,34 +9,38 @@ import subprocess
 import bezelie
 
 csvFile = "bezeTalk.csv"
+awakingTime = 7
+sleepingTime = 24
 
 # Definition
 def timeMessage(timeSlot):
   data = []
-  with open(csvFile, 'rb') as f:  # open file
-    for i in csv.reader(f):       #
-      data.append(i)              #
+  with open(csvFile, 'rb') as f:  # opening the file to read
+    for i in csv.reader(f):       
+      data.append(i)              
 
   data1 = []
-  for index,i in enumerate(data): #
-    if i[1]==timeSlot:            #
-      j = int(i[3])/(int(i[4])+randint(1,2))
+  for index,i in enumerate(data): # making candidate list
+    if i[1]==timeSlot:            
+      j = int(i[3])*randint(9,11)
       data1.append(i+[j]+[index])
 
   maxNum = 0
-  for i in data1:
+  for i in data1:                 # decision
     if i[5] > maxNum:
       maxNum = i[5]
       ansNum = i[6]
 
-  data[ansNum][4] = int(data[ansNum][4])+1
+  data[ansNum][4] = int(data[ansNum][4])+1 # adding counter
 
-  with open(csvFile, 'wb') as f:
+  with open(csvFile, 'wb') as f:  # opening the file to overwrite
     csv.writer(f).writerows(data)
 
+  # AquesTalk
   print data[ansNum][2]
-  subprocess.call('/home/pi/aquestalkpi/AquesTalkPi -s 120 "'+ data[ansNum][2] +'" | aplay&', shell=True)
   bezelie.movePit (20, 1)
+  sleep(2)
+  subprocess.call('/home/pi/aquestalkpi/AquesTalkPi -s 120 "'+ data[ansNum][2] +'" | aplay', shell=True)
   sleep(2)
   bezelie.movePit (0, 1)
   sleep(2)
@@ -48,31 +52,38 @@ bezelie.centering()
 try:
   while (True):
     now = datetime.datetime.now()
-    if now.hour < 8:                # mid night
-      pass
-#      sleep(60)
-    elif now.hour == 8:
+    print now
+    if now.hour < awakingTime:
+      print "It is a midnight"
+      sleep(60)
+    elif now.hour == awakingTime:
+      print "It is an awaking time"
       timeMessage("awaking")
-#      sleep(randint(60*20,60*40))
+      sleep(randint(60*10,60*20))
     elif now.hour < 12:
+      print "It is a morning"
       timeMessage("morning")
-#      sleep(randint(60*50,60*70))
+      sleep(randint(60*50,60*70))
     elif now.hour == 12:
+      print "It is about noon"
       timeMessage("noon")
-#      sleep(randint(60*20,60*40))
+      sleep(randint(60*20,60*40))
     elif now.hour < 16:
+      print "It is an afternoon"
       timeMessage("afternoon")
-#      sleep(randint(60*110,60*130))
+      sleep(randint(60*110,60*130))
     elif now.hour < 19:
+      print "It is an evening"
       timeMessage("evening")
-#      sleep(randint(60*50,60*70))
-    elif now.hour < 23:
+      sleep(randint(60*50,60*70))
+    elif now.hour < sleepingTime-1:
+      print "It is a night"
       timeMessage("night")
-#      sleep(randint(60*50,60*70))
+      sleep(randint(60*50,60*70))
     else:
+      print "It is a time to go to the bed"
       timeMessage("bedtime")
-#      sleep(randint(60*20,60*40))
-    sleep(1)
+      sleep(randint(60*20,60*40))
 
 except KeyboardInterrupt:
   print " Interrupted by Keyboard"
