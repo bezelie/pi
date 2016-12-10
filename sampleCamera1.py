f0244c2bbbd9217ea952736ee91bed6e4a44f0a8
bezelie <nuja@live.jp>
2016-12-10 17:54:08 +0900

    Dec10

--- sampleCamera1.py
+++ sampleCamera1.py
@@ -6,6 +6,7 @@ import bezelie
 
 # Centering All Servos
 bezelie.centering()
+sleep (1)
 
 # Main Loop
 try:
@@ -13,20 +14,22 @@ try:
     camera.resolution = (800, 480)   # change this number depending on your display
     camera.rotation = 180            # comment out if your screen is upside down
     camera.start_preview()
-    sleep(0.2)
+    sleep(2)
     pit = 0
     while (True):
-      bezelie.moveRot (-20)
+      bezelie.moveRot (-10)
+      sleep (0.5)
       bezelie.moveYaw (-40, 2)
       sleep (1)
-      bezelie.moveRot (20)
+      bezelie.moveRot ( 10)
+      sleep (0.5)
       bezelie.moveYaw (40, 2)
       sleep (1)
       pit += 10
       if pit > 20:
         pit = -20
       bezelie.movePit (pit)
-      sleep (1)
+      sleep (0.5)
 
 except KeyboardInterrupt:
   print " Interrupted by Keyboard"
